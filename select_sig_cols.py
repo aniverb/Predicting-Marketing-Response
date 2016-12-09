cf998a45649efc7ff37812d56025b75c359b81b7
Nole Lin <nole.lin@gmail.com>
2016-12-08 23:20:53 -0500

    Added ID and target vars

--- select_sig_cols.py
+++ select_sig_cols.py
@@ -7,6 +7,8 @@ def main():
     sig_num = pd.read_csv(os.path.expanduser('~/Downloads/sig_numer_col.csv'))
     sig_cat = pd.read_csv(os.path.expanduser('~/Downloads/catSigColFisher.csv'))
     sig_cols = list(sig_num.columns.values) + list(sig_cat['x'])
+    sig_cols = ['ID'] + sig_cols
+    sig_cols.append('target')
     newtrain = train[sig_cols]
     newtrain.to_csv(os.path.expanduser('~/Downloads/train_split/train_a_sig.csv'), index=False)
 main()
