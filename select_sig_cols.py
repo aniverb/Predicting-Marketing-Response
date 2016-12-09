a58e536378d66c621c72c0708d7bc8d415be75d4
Nole Lin <nole.lin@gmail.com>
2016-12-08 23:08:05 -0500

    Selecting sig cols

--- select_sig_cols.py
+++ select_sig_cols.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 
 def main():
-    train = pd.read_csv(os.path.expanduser('~/Downloads/train_split/train_a_no_dup'))
+    train = pd.read_csv(os.path.expanduser('~/Downloads/train_split/train_a_no_dup.csv'))
     sig_num = pd.read_csv(os.path.expanduser('~/Downloads/sig_numer_col.csv'))
     sig_cat = pd.read_csv(os.path.expanduser('~/Downloads/catSigColFisher.csv'))
     sig_cols = list(sig_num.columns.values) + list(sig_cat['x'])
